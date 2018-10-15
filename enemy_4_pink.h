e4a108b5cf65f605650b442be2c60c3cf7dc7a05
alvin5716 <alvin5716@yahoo.com>
2018-10-15 15:24:42 +0800

    Revert "enemy ctor adjust"
    This reverts commit f781afd7fa64f000f6fdec2a955a8b11716aae0a.

--- enemy_4_pink.h
+++ enemy_4_pink.h
@@ -6,7 +6,7 @@
 class Enemy_4_Pink: public Enemy_4
 {
 public:
-    Enemy_4_Pink(Character* player, int health, int radius, int shoot_cd, int shoot_cd_init, double x=0, double y=0, double xv=0, double yv=0, double xa=0, double ya=0, bool stopable=false);
+    Enemy_4_Pink(QString img, int img_w, int img_h, int show_w, int show_h, Character* player, int health, int radius, int shoot_cd, int shoot_cd_init, double x=0, double y=0, double xv=0, double yv=0, double xa=0, double ya=0, bool stopable=false);
     void attacked() override;
     void skill() override;
 private:
--- enemy_4_red.h
+++ enemy_4_red.h
@@ -6,7 +6,7 @@
 class Enemy_4_Red: public Enemy_4
 {
 public:
-    Enemy_4_Red(Character* player, int health, int radius, int shoot_cd, int shoot_cd_init, double x=0, double y=0, double xv=0, double yv=0, double xa=0, double ya=0, bool stopable=false);
+    Enemy_4_Red(QString img, int img_w, int img_h, int show_w, int show_h, Character* player, int health, int radius, int shoot_cd, int shoot_cd_init, double x=0, double y=0, double xv=0, double yv=0, double xa=0, double ya=0, bool stopable=false);
     void skill() override;
     std::vector<Bullet*>* shoot() override;
 };
